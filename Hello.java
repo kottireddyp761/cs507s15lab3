372357294dff86b6c2ee35b82108b41f1af90134
madhavarama257 <madhavarama257@github.com>
2015-10-05 18:29:53 -0400

    divisioin added

--- Hello.java
+++ Hello.java
@@ -5,18 +5,19 @@ class Hello
 {
   public static void main(String args[])
   {
-      int a,b,c,difference,multiplication;
+      int a,b,c,difference,multiplication,div;
       a= 5;
       b=4;
       c=a+b;
       difference = a - b ;
       multiplication = a*b;
+      div = a/b;
       
       System.out.println("the sum of it is"+c);//prints the sum of two integers
   System.out.println("Hello ! Welcome all to GitHub");
   System.out.println(" the multiplication of two integers:");
   // prints the message 
   System.out.println("Difference of 2 numbers" + difference);
-
+  System.out.println("division of two numbers is" + div);
   }
 }
