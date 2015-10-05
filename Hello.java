f3fa2da4e0b3df877201fac409d9de978aad8ffa
kottireddyp761 <kottireddyp761@github.com>
2015-10-05 14:32:11 -0400

    Entered the Code for difference of 2 numbers

--- Hello.java
+++ Hello.java
@@ -5,12 +5,15 @@ class Hello
 {
   public static void main(String args[])
   {
-      int a,b,c;
+      int a,b,c,d;
       a= 5;
       b=4;
       c=a+b;
+      d = a -  b;
+      System.out.println(" the diffence of d " d);// prints the difference of two integers
       System.out.println("the sum of it is"+c);//prints the sum of two integers
   System.out.println("Hello ! Welcome all to GitHub");
   // prints the message in the parenthesis
+
   }
 }
