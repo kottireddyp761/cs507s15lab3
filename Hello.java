01813b1abb5cd0f0c6d66ff2cde0b33817e2bb06
kottireddyp761 <kottireddyp761@github.com>
2015-10-05 15:02:52 -0400

    print multiplication of two integers

--- Hello.java
+++ Hello.java
@@ -5,13 +5,16 @@ class Hello
 {
   public static void main(String args[])
   {
-      int a,b,c,difference;
+      int a,b,c,difference,multiplication;
       a= 5;
       b=4;
       c=a+b;
       difference = a - b ;
+      multiplication = a*b;
+      
       System.out.println("the sum of it is"+c);//prints the sum of two integers
   System.out.println("Hello ! Welcome all to GitHub");
+  System.out.println(" the multiplication of two integers:");
   // prints the message 
   System.out.println("Difference of 2 numbers" + difference);
 
