a3cea4e720d310dc9a86a2b08a3ae633e2b7b270
chintalapatis243 <chintalapatis243@github.com>
2015-10-05 14:22:39 -0400

    Difference code is added

--- Hello.java
+++ Hello.java
@@ -5,10 +5,11 @@ class Hello
 {
   public static void main(String args[])
   {
-      int a,b,c;
+      int a,b,c,difference;
       a= 5;
       b=4;
       c=a+b;
+      difference = a - b ;
       System.out.println("the sum of it is"+c);//prints the sum of two integers
   System.out.println("Hello ! Welcome all to GitHub");
   // prints the message in the parenthesis
