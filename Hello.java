43a1faf6711e8da87a3dce4890d2067a927e3b6b
thaduris091 <thaduris091@github.com>
2015-10-05 12:58:09 -0400

    changes made for adding two numbers

--- Hello.java
+++ Hello.java
@@ -1,8 +1,16 @@
+
+import java.util.*;
+
 class Hello
 {
   public static void main(String args[])
   {
-  system.out.println("Hello ! Welcome all to GitHub");
+      int a,b,c;
+      a= 5;
+      b=4;
+      c=a+b;
+      System.out.println("the sum of it is"+c);//prints the sum of two integers
+  System.out.println("Hello ! Welcome all to GitHub");
   // prints the message in the parenthesis
   }
 }
